047a5da39a0f8da4ae557c4cfb42128f756362c0
celine24 <peyrot.celine@gmail.com>
2016-05-24 11:32:33 +0200

    footer

--- resources/views/front/components/footer.blade.php
+++ resources/views/front/components/footer.blade.php
@@ -1,4 +1,27 @@
 <footer>
-
-
+    <div class="mdp-footer">
+        <div class="mdp-footer-menu">
+            <ul class="mdp-footer-main-menu">
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Institut</a></li>
+                <li><a href="">Prestations</a></li>
+                <li><a href="">Produits</a></li>
+                <li><a href="">Offres</a></li>
+                <li><a href="">Conseils</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+            <ul class="mdp-footer-small-menu">
+                <li><a href="">Partenaires</a></li>
+                <li><a href="">Mentions légales</a></li>
+                <li><a href="">Remerciements</a></li>
+            </ul>
+        </div>
+        <div class="mdp-footer-fb">
+            <p>Retrouvez Ma Douce Parenthèse sur Facebook</p>
+            <p class="mdp-footer-fb-icons">
+                <img src="{{ asset('img/footer/facebook.png') }}" alt="facebook" />
+                <img src="{{ asset('img/footer/like.png') }}" alt="like" />
+            </p>
+        </div>
+    </div>
 </footer>
