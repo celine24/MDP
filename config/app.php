16896d88203940df5b0188784ad849efc52a496d
celine24 <peyrot.celine@gmail.com>
2016-06-10 14:36:32 +0200

    ajout class Form

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class, /* - package pour formulaire(entre autres)
 
         /*
          * Application Service Providers...
@@ -156,7 +157,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Bestmomo\Scafold\ScafoldServiceProvider::class,
+        Bestmomo\Scafold\ScafoldServiceProvider::class, //package pour l'authentification
 
     ],
 
@@ -203,6 +204,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
